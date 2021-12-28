f98b890bb55e7e3675c2ca005a86849bb3d7f9c1
dogfootman <idogfootman@gmail.com>
2021-12-28 16:57:23 +0900

    init set google-services.json add. pubspec.yaml modified android/build.gradle modified android/app/build.gradle modified

---
+++ lib/model/itJargon.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class ItJargon {
+  // int? id;
+  // String keyword;
+  // String description;
+  // String[] tags;
+  //
+  // ItJargon({
+  //   this.keyword, this.description, this.tags
+  // });
+
+  // ItJargon.fromSnapshot(DataSnaption snapshot)
+}
